2c3d27faea42e0ab63f1dd39adfbfa22b239cc8b
wanhuasong <wanhuasong@qq.com>
2023-03-21 16:11:02 +0800

    update: set EncodeTime
    Signed-off-by: wanhuasong <wanhuasong@qq.com>

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/juju/errors"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
@@ -18,26 +20,34 @@ var (
 
 	logFile = filepath.Join(os.TempDir(), "webhook.log")
 
-	logger *zap.Logger
+	log *zap.Logger
+
+	broken bool
 )
 
 // https://willh.gitbook.io/build-web-application-with-golang-zhtw/03.0/03.2
 
 func handleEvent(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
-		logger.Warn("invalid request method", zap.String("method", req.Method))
+		log.Warn("invalid request method", zap.String("method", req.Method))
 		return
 	}
 
 	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		logger.Error("read body failed", zap.Error(err))
+		log.Error("read body failed", zap.Error(err))
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
 
 	count++
-	logger.Info("receive a message", zap.String("message", string(b)), zap.Int64("count", count))
+	log.Info("receive a message", zap.String("message", string(b)), zap.Int64("count", count))
+
+	if broken {
+		log.Warn("return 500")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
@@ -46,7 +56,7 @@ func handleEvent(w http.ResponseWriter, req *http.Request) {
 func readLog(w http.ResponseWriter, req *http.Request) {
 	b, err := ioutil.ReadFile(logFile)
 	if err != nil {
-		logger.Error("read log file failed", zap.Error(err))
+		log.Error("read log file failed", zap.Error(err))
 		http.Error(w, "can't read log file", http.StatusInternalServerError)
 		return
 	}
@@ -62,20 +72,36 @@ func initRouter() {
 
 func run() {
 	addr := fmt.Sprintf(":%d", port)
-	logger.Info("running", zap.String("addr", addr))
+	log.Info("running", zap.String("addr", addr))
 
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
-		logger.Fatal("start failed", zap.Error(err))
+		log.Fatal("start failed", zap.Error(err))
 	}
+}
+
+func encodeTimeLayout(t time.Time, layout string, enc zapcore.PrimitiveArrayEncoder) {
+	type appendTimeEncoder interface {
+		AppendTimeLayout(time.Time, string)
+	}
+
+	if enc, ok := enc.(appendTimeEncoder); ok {
+		enc.AppendTimeLayout(t, layout)
+		return
+	}
+
+	enc.AppendString(t.Format(layout))
 }
 
 func initLog() (func(), error) {
 	logCfg := zap.NewProductionConfig()
 	logCfg.OutputPaths = append(logCfg.OutputPaths, logFile)
+	logCfg.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		encodeTimeLayout(t, "2006-01-02 15:04:05.000", enc)
+	}
 
 	var err error
-	logger, err = logCfg.Build()
+	log, err = logCfg.Build()
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -88,12 +114,13 @@ func initLog() (func(), error) {
 	// log.SetOutput(f)
 
 	return func() {
-		logger.Sync()
+		log.Sync()
 	}, nil
 }
 
 func initFlags() {
 	flag.IntVar(&port, "port", 9000, "port")
+	flag.BoolVar(&broken, "broken", false, "broken")
 	flag.Parse()
 }
 
@@ -106,7 +133,7 @@ func main() {
 	}
 	defer f()
 
-	logger.Info("log file created", zap.String("file", logFile))
+	log.Info("log file created", zap.String("file", logFile))
 
 	initRouter()
 	run()
